e40ec8634bbad635bace3f84e0abc708ba8fb3c5
unknown <casilva16@uc.cl>
2021-04-11 18:11:10 -0400

    2commit

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    #path('breakingbad/', views.breakingbad, name='breakingbad'),
     path('episode_bcs/int:<episode_id>', views.episode_bcs, name='episode_bcs'), 
     path('character/int:<char_id>', views.character, name='character'),
+    path('search_results/', views.search_results, name='search_results'),
+    path('episodesbb/int:<i>', views.episodesbb, name='episodesbb'),
+    path('episodesbcs/int:<j>', views.episodesbcs, name='episodesbcs'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -10,14 +10,54 @@ def index(request):
     response2 = requests.get('https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=Breaking+Bad')
     databb = response2.json()
 
-    s1bb, s2bb, s3bb, s4bb, s5bb,  = make_seasons(databb, True)
+    s1bb, s2bb, s3bb, s4bb, s5bb  = make_seasons(databb, True)
     s1bcs, s2bcs, s3bcs, s4bcs, s6bcs = make_seasons(databcs, False)
-
-    ctx = {'s1': s1bcs, 's2': s2bcs, 's3': s3bcs, 's4': s4bcs, 's6': s6bcs,
-    's1bb': s1bb, 's2bb': s2bb, 's3bb': s3bb, 's4bb': s4bb, 's5bb': s5bb
-    }
-    return render(request, 'index.html', ctx)
+    rango = [1, 2, 3, 4, 5]
+    rango2 = [1, 2, 3, 4, 6]
+    #ctx = {'s1': s1bcs, 's2': s2bcs, 's3': s3bcs, 's4': s4bcs, 's6': s6bcs,
+    #'s1bb': s1bb, 's2bb': s2bb, 's3bb': s3bb, 's4bb': s4bb, 's5bb': s5bb, 'range': range(5)
+    #}
+    ctx2 = {'range': rango, 'range2': rango2}
+    return render(request, 'index.html', ctx2)
     
+
+def episodesbb(request, i):
+    response2 = requests.get('https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=Breaking+Bad')
+    databb = response2.json()
+    s1bb, s2bb, s3bb, s4bb, s5bb  = make_seasons(databb, True)
+
+    if int(i) == 1:
+        ctx = {'season': s1bb, 'n': 1}
+    elif int(i) == 2:
+        ctx = {'season': s2bb, 'n': 2}
+    elif int(i)  == 3:
+        ctx = {'season': s3bb, 'n': 3}   
+    elif int(i)  == 4:
+        ctx = {'season': s4bb, 'n': 4}
+    elif int(i) == 5:
+        ctx = {'season': s5bb, 'n': 5}
+
+    return render(request, 'episodesbb.html', ctx)
+
+
+def episodesbcs(request, j): 
+    response = requests.get('https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=Better+Call+Saul')
+    databcs = response.json()
+    s1bcs, s2bcs, s3bcs, s4bcs, s6bcs = make_seasons(databcs, False)
+    
+    if int(j) == 1:
+        ctx = {'seasonbcs': s1bcs, 'c': 1}
+    elif int(j) == 2:
+        ctx = {'seasonbcs': s2bcs, 'c': 2}
+    elif int(j)  == 3:
+        ctx = {'seasonbcs': s3bcs, 'c': 3}   
+    elif int(j)  == 4:
+        ctx = {'seasonbcs': s4bcs, 'c': 4}
+    elif int(j) == 5:
+        ctx = {'seasonbcs': s6bcs, 'c': 6}
+    
+    return render(request, 'episodesbcs.html', ctx)
+
 
 def episode_bcs(request, episode_id):
     response = requests.get('https://tarea-1-breaking-bad.herokuapp.com/api/episodes/'+episode_id)
@@ -34,8 +74,25 @@ def character(request, char_id):
     response = requests.get('https://tarea-1-breaking-bad.herokuapp.com/api/characters/'+char_id)
     data = response.json()
     personaje = data[0]
+    quotes = []
+    response2 = requests.get('https://tarea-1-breaking-bad.herokuapp.com/api/quotes')
+    data2 = response2.json()
+    for quote in data2:
+        if quote['author'] == personaje['name']:
+           quotes.append(quote)
 
-    return render(request, 'character.html', {'personaje': personaje})
+    ctx = {'personaje': personaje, 'quotes': quotes}
+    return render(request, 'character.html', ctx)
+
+
+def search_results(request, name): # recibe letra o nombre
+    responde = response = requests.get('https://tarea-1-breaking-bad.herokuapp.com/api/characters/')
+    data = response.json()
+
+    for personaje in data:
+        if name in personaje.name:
+            ctx = {'personaje': personaje}
+            return render(request, 'search_results.html', ctx)
 
 
 def make_seasons(data, valor):
